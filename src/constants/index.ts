43e0c56012f36f4e6042e8d2b0dfb2bdcd433af9
Dharmendra Kashaudhan <kashaudhan94@gmail.com>
2024-05-25 11:02:27 +0530

    chore: Added reactflow provider

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+
+// local storage key
+export const FLOW_KEY = "flow-key";
+
+// initial node info
+export const initialNodes = [
+  {
+    id: "1",
+    type: "messageNode",
+    data: { label: "input nodes" },
+    position: { x: 250, y: 5 },
+  },
+];
+
+export const flowStyle = {
+  backgroundColor: "#ffffff",
+};
