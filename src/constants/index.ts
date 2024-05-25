738bf0462096772cc09282363518a066d5f31aa7
Dharmendra Kashaudhan <kashaudhan94@gmail.com>
2024-05-25 19:20:52 +0530

    fix: initial node id

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -5,7 +5,7 @@ export const FLOW_KEY = "flow-key";
 // initial node info
 export const initialNodes = [
   {
-    id: "1",
+    id: "node_0",
     type: "messageNode",
     data: { label: "new message node" },
     position: { x: 250, y: 5 },
